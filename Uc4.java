0286a5ef0a0406aa4897a550f93ae2abf60a1f0a
pinkul1234 <78338132+pinkul1234@users.noreply.github.com>
2022-04-23 00:04:35 +0530

    Update Uc4.java

--- Uc4.java
+++ Uc4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddressBookMain {
+public class Uc4 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address Book");
